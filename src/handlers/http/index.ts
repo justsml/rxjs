b9d8fd597c516e9313dbfab50d3ad57b8030d00c
Dan Levy <397632+justsml@users.noreply.github.com>
2023-04-12 15:46:24 -0600

    Refactored to pure Promises

--- src/handlers/http/index.ts
+++ src/handlers/http/index.ts
@@ -7,47 +7,35 @@ import { withLatestFrom, zip } from 'rxjs';
 import { People, Starship } from '@rx-example/types';
 
 // This creates an array of Promises that will call SWAPI to get people
-const getNPeoplePromises = (n) =>
-  map(range(1, n), async (i) => {
-    console.warn(i, 'getting person...');
-    const response = await axios.get<People>(
-      `https://swapi.dev/api/people/${i}`,
-    );
-    console.warn(i, '...person got');
-    return response.data;
-  });
+const getNPeoplePromises = (n) => range(1, n).map(getPerson);
+
+const getPerson = (i) => {
+  console.warn(i, 'getting person...');
+  return axios.get<People>(
+    `https://swapi.dev/api/people/${i}`,
+  ).then(response => response.data)
+}
 
 // This creates a single Promise that will get a starship
-const getMyStarship = () =>
-  new Promise<Starship>(async (resolve) => {
-    console.warn('Getting our starship...');
-    const response = await axios.get<Starship>(
-      'https://swapi.dev/api/starships/10/',
-    );
-    const starship = response.data;
-    console.warn('...starship got');
-    resolve(starship);
-  });
+const getMyStarship = () => {
+  console.warn('Getting our starship...');
+  return axios.get<Starship>('https://swapi.dev/api/starships/10/')
+    .then(({data}) => {
+      console.warn('...starship got');
+      return data;
+    })
+});
 
 const transform = (people: People[], starship: Starship): Starship => ({
   ...starship,
   passengers: people,
 });
 
-export const getCraftAndCrew = () =>
-  new Promise((resolve) => {
-    // 1. We zip all our promises up to create one Observable
-    zip(...getNPeoplePromises(10))
-      .pipe(
-        // 2. we operate over the Observables
-        withLatestFrom(getMyStarship()), // 3. Waits to finish until it gets the latest from this call
-      )
-      .subscribe(([people, starship]: [People[], Starship]) => {
-        // 4. Now we have everything we need
-        const starshipWithCrew = transform(people, starship);
-        resolve(starshipWithCrew);
-      });
-  });
+export const getCraftAndCrew = () => Promise.all([
+    getMyStarship(),
+    ...getNPeoplePromises(10)
+  ])
+  .then(([starship, ...people]: [Starship, People[]]) => transform(people, starship));
 
 export const handler: APIGatewayProxyHandler = async (_event, _context) => {
   const craftAndCrew = await getCraftAndCrew();
